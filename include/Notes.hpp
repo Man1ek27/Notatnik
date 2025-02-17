4e12999cad5dd61b093171e7bf39a9cfaef8b924
Mariusz Wątroba <mariusz2723@gmail.com>
2025-02-17 16:36:14 +0100

    Added documentation

--- include/Notes.hpp
+++ include/Notes.hpp
@@ -16,15 +16,28 @@ class Notes{
 
     public:
         Notes() = default;
+
+        //Insterts path to file
         Notes(std::string);
 
+        //Gives kursor animation 
         void Kursor_print();
+
+        //Moving kursor (up, down, left, right)
+        //parm int - inserts asci of key
         void Kursor_move(int);
+
+        //other opertaions
+        //parm int - inserts asci of key
         void Operation(int);
+
+        //Prints menu
         void Print_menu()const;
         
+        //Saving to file
         void Save();
 
+        //Wypisywanie na ekran tekstu
         void Print()const;
         
 
